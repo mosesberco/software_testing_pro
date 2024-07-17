14996d96f8502783728b52d5aecd43679e28eb83
moses <mosesberco1@gmail.com>
2024-07-17 23:56:44 +0300

    adding the store code

---
+++ Itemstore.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testing_project
+{
+    internal class Itemstore
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string ImagePath { get; set; }
+
+        public Itemstore(string name, decimal price, string imagePath)
+        {
+            Name = name;
+            Price = price;
+            ImagePath = imagePath;
+        }
+        public override string ToString()
+        {
+            return $"{Name}: ${Price}";
+        }
+    }
+}
